4dc86323fa27b85688a42745a4dc1697a066f52b
Matt Kornfield <kornfield@gretel.ai>
2022-12-13 22:25:18 -0800

    really basic stoof

--- loadFitness.js
+++ loadFitness.js
@@ -1,1 +1,5 @@
-console.log("Testing!")
+document.getElementById("play-button").addEventListener("click", () => {
+    const words = document.getElementById("stuff-to-say").textContent
+    const utterance = new SpeechSynthesisUtterance(words);
+    speechSynthesis.speak(utterance);
+})
